1e5f511d3586abbd326a47cd1b474216915676c1
Amanda <haartemis.amir@gmail.com>
2020-02-17 19:06:14 +0200

    Initial commit. Borders made and world filled with 20 rectangles.

---
+++ script.js
@@ -0,0 +1,64 @@
+const {
+	Engine,
+	Render,
+	Runner,
+	World,
+	Bodies,
+	MouseConstraint,
+	Mouse
+} = Matter;
+
+const engine = Engine.create();
+
+const { world } = engine;
+
+const width = 800;
+const height = 600;
+
+const render = Render.create({
+	element: document.body,
+	engine: engine,
+	options: {
+		width,
+		height
+	}
+});
+
+Render.run(render);
+
+Runner.run(Runner.create(), engine);
+
+World.add(
+	world,
+	MouseConstraint.create(engine, {
+		mouse: Mouse.create(render.canvas)
+	})
+);
+
+//Borders
+
+const borders = [
+	Bodies.rectangle(400, 600, 800, 100, {
+		isStatic: true
+	}),
+	Bodies.rectangle(400, 0, 800, 100, {
+		isStatic: true
+	}),
+	Bodies.rectangle(0, 300, 100, 600, {
+		isStatic: true
+	}),
+	Bodies.rectangle(800, 300, 100, 600, {
+		isStatic: true
+	})
+];
+
+World.add(world, borders);
+
+//Random shapes
+
+for (let i = 0; i < 20; i++) {
+	World.add(
+		world,
+		Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
+	);
+}
